6d54bd21b6f3beea19801b68f64a4fcf6b1224f0
MikeDev96 <trikefx@live.co.uk>
2020-10-27 22:29:34 +0000

    ~ Renamed server.js to index.js ~ Modified pm2 config file.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [
     {
-      name: "ytd-server",
-      script: "./server.js",
+      name: "youtube-downloader",
+      script: "./index.js",
       out_file: "./logs/out.log",
       error_file: "./logs/err.log",
       time: true,
